c0dc8f2677e8164f0c656655dd6858704425a94a
Huangdi-599 <adebayoh76@gmail.com>
2023-04-24 18:26:34 -0700

    fixed error

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -10,11 +10,11 @@ if __name__ == "__main__":
     tasks = requests.get(url + "todos", params={"userId": sys.argv[1]}).json()
     user_id = sys.argv[1]
     username = employers.get("username")
-    completed = [task.get("title") 
+    completed = [task.get("title")
                  for task in tasks if task.get("completed") is True]
 
     with open('{}.csv'.format(sys.argv[1]), mode='w', newline="") as csv_file:
-        writer = csv.writer(csv_file,quoting=csv.QUOTE_ALL)
+        writer = csv.writer(csv_file, quoting=csv.QUOTE_ALL)
         [writer.writerow(
             [user_id, username, t.get("completed"), t.get("title")])
          for t in tasks]
